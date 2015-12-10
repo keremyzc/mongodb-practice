e6a5c2c9450df95d7f5fac6facf26e0709ea13f2
keremyzc <keremyazici@gmail.com>
2015-12-10 18:11:37 +0000

    created a new test class for the  collection operations

---
+++ src/test/java/com/yazici/mongodb/practice/MongoCollectionOperationTest.java
@@ -0,0 +1,62 @@
+package com.yazici.mongodb.practice;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.result.DeleteResult;
+import com.yazici.mongodb.practice.bind.ConfigModule;
+import com.yazici.mongodb.practice.bind.MongoClientModule;
+import org.bson.Document;
+import org.junit.*;
+
+import java.util.Arrays;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNull.nullValue;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by keremyzc on 08/12/2015.
+ */
+public class MongoCollectionOperationTest {
+
+
+    public static final String DATABASE_NAME = "kytestdb";
+    public static final String COLLECTION_NAME = "testCollection";
+
+    private static Injector injector;
+    private static MongoClient mongoClient;
+    private static MongoDatabase kytestdb;
+    private Document testUser1;
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+        injector = Guice.createInjector(new ConfigModule(), new MongoClientModule());
+        mongoClient = injector.getInstance(MongoClient.class);
+        kytestdb = mongoClient.getDatabase(DATABASE_NAME);
+
+    }
+
+
+    @Before
+    public void insertSomeTestData() {
+        testUser1 = new Document("name", "john")
+                .append("age", 33)
+                .append("phone", "0983042");
+    }
+
+    @After
+    public void deleteTestData() {
+    }
+
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+        mongoClient.close();
+        injector = null;
+    }
+
+}
